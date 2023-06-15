69fa8ca28d82057d88611cb8a9b2d7fb95cccf52
aqulline <aqeglipambuya@gmail.com>
2023-06-15 13:35:33 +0300

    now starting QRCODe scan

---
+++ database.py
@@ -0,0 +1,190 @@
+import datetime
+
+from kivymd.toast import toast
+
+
+class DataBase:
+    mnth_name = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                 "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    day = ["Mon", "Tue", "Wed", "Thu",
+           "Fri", "Sat", "Sun"]
+
+    def save_product(self, product_name, product_price, product_quantity, product_barcode, product_exp_year,
+                     product_exp_month):
+        if True:
+            import firebase_admin
+            firebase_admin._apps.clear()
+            from firebase_admin import credentials, initialize_app, db
+            if not firebase_admin._apps:
+                cred = credentials.Certificate("credential/farmzon-abdcb-c4c57249e43b.json")
+                initialize_app(cred, {'databaseURL': 'https://farmzon-abdcb.firebaseio.com/'})
+                data = {
+                    "product_name": product_name,
+                    "product_price": product_price,
+                    "product_quantity": product_quantity,
+                    "product_barcode": product_barcode,
+                    "product_expire": f"{product_exp_year}-{product_exp_month}",
+                    "days_to_exp": self.day_remain(f"{product_exp_year}-{product_exp_month}")
+                }
+                ref = db.reference('ShopCode').child("Products").child(product_barcode)
+                ref.set(data)
+                toast("Saved!")
+
+    def get_product(self, product_id):
+        if True:
+            import firebase_admin
+            firebase_admin._apps.clear()
+            from firebase_admin import credentials, initialize_app, db
+            if not firebase_admin._apps:
+                cred = credentials.Certificate("credential/farmzon-abdcb-c4c57249e43b.json")
+                initialize_app(cred, {'databaseURL': 'https://farmzon-abdcb.firebaseio.com/'})
+                ref = db.reference('ShopCode').child("Products").child(product_id)
+                data = ref.get()
+
+                return data
+
+    def sell_product(self, sold_amount, sold_quantity, product_id, name):
+        data = self.get_product(product_id)
+        today_data = self.get_today(product_id)
+        self.today_sell(today_data, sold_amount, sold_quantity, product_id, name)
+        if "sold_amount" not in data:
+            trans_data = {
+                "sold_amount": sold_amount,
+                "sold_quantity": sold_quantity,
+                "number_sold": 1,
+            }
+
+            self.sell_product_trans(trans_data, product_id)
+        else:
+            sold_amount = str(int(data["sold_amount"].replace("/=", "")) + int(sold_amount.replace("/=", "")))
+            sold_quantity = str(int(data["sold_quantity"]) + int(sold_quantity))
+            number_sold = str(int(data["number_sold"]) + 1)
+            trans_data = {
+                "sold_amount": sold_amount,
+                "sold_quantity": sold_quantity,
+                "number_sold": number_sold
+            }
+            self.sell_product_trans(trans_data, product_id)
+
+
+
+    def today_sell(self, today_data, sold_amount, sold_quantity, product_id, name):
+        if today_data:
+            sold_amount = str(int(today_data["sold_amount"].replace("/=", "")) + int(sold_amount.replace("/=", "")))
+            sold_quantity = str(int(today_data["sold_quantity"]) + int(sold_quantity))
+            number_sold = str(int(today_data["number_sold"]) + 1)
+            trans_data = {
+                "sold_amount": sold_amount,
+                "sold_quantity": sold_quantity,
+                "number_sold": number_sold,
+                "product_name": name
+            }
+            self.sell_product_today(product_id, trans_data)
+        else:
+            trans_data = {
+                "sold_amount": sold_amount,
+                "sold_quantity": sold_quantity,
+                "number_sold": 1,
+                "product_name":name
+            }
+            self.sell_product_today(product_id, trans_data)
+
+
+
+
+
+    def sell_product_trans(self, data, product_id):
+        if True:
+            import firebase_admin
+            firebase_admin._apps.clear()
+            from firebase_admin import credentials, initialize_app, db
+            if not firebase_admin._apps:
+                cred = credentials.Certificate("credential/farmzon-abdcb-c4c57249e43b.json")
+                initialize_app(cred, {'databaseURL': 'https://farmzon-abdcb.firebaseio.com/'})
+                ref = db.reference('ShopCode').child("Products").child(product_id)
+                ref.update(data)
+
+    def sell_product_today(self, product_id, data):
+        if True:
+            import firebase_admin
+            firebase_admin._apps.clear()
+            from firebase_admin import credentials, initialize_app, db
+            if not firebase_admin._apps:
+                cred = credentials.Certificate("credential/farmzon-abdcb-c4c57249e43b.json")
+                initialize_app(cred, {'databaseURL': 'https://farmzon-abdcb.firebaseio.com/'})
+                ref = db.reference('ShopCode').child("Sold").child(str(datetime.datetime.now().date().year)).child(
+                    self.date_format()).child(product_id)
+                ref.set(data)
+
+    def get_today(self, product_id):
+        if True:
+            import firebase_admin
+            firebase_admin._apps.clear()
+            from firebase_admin import credentials, initialize_app, db
+            if not firebase_admin._apps:
+                cred = credentials.Certificate("credential/farmzon-abdcb-c4c57249e43b.json")
+                initialize_app(cred, {'databaseURL': 'https://farmzon-abdcb.firebaseio.com/'})
+                try:
+                    ref = db.reference('ShopCode').child("Sold").child(str(datetime.datetime.now().date().year)).child(self.date_format()).child(product_id)
+                    data = ref.get()
+
+                    return data
+                except:
+
+                    return False
+
+    def get_all_today(self):
+        if True:
+            import firebase_admin
+            firebase_admin._apps.clear()
+            from firebase_admin import credentials, initialize_app, db
+            if not firebase_admin._apps:
+                cred = credentials.Certificate("credential/farmzon-abdcb-c4c57249e43b.json")
+                initialize_app(cred, {'databaseURL': 'https://farmzon-abdcb.firebaseio.com/'})
+                try:
+                    ref = db.reference('ShopCode').child("Sold").child(str(datetime.datetime.now().date().year)).child(self.date_format())
+                    data = ref.get()
+
+                    return data
+                except:
+
+                    return False
+
+
+    def get_date(self):
+        return str(datetime.datetime.now()).split(" ")[0]
+
+    def date_format(self):
+        date = self.get_date()
+        year, month, day = date.strip().split("-")
+        if int(month) >= 10:
+            month_update = int(month) - 1
+        else:
+            month_update = int(month.replace("0", "")) - 1
+        month_name = self.mnth_name[month_update]
+        date_frmt = f"{month_name} {str(day)}"
+        return date_frmt
+
+    def day_remain(self, exp_date):
+        nowoy = datetime.datetime.now().date().year
+        nowom = datetime.datetime.now().date().month
+
+        now = f"{nowoy}-{nowom}"
+
+        m1 = int(now.strip().split("-")[1])
+
+        m2 = int(exp_date.strip().split("-")[1])
+
+        y1 = int(now.strip().split("-")[0])
+
+        y2 = int(exp_date.strip().split("-")[0])
+
+        yd = (y2 - y1) * 365
+
+        ytm1 = 30 * m1
+
+        ytm2 = 30 * m2
+
+        v = ytm2 - ytm1 + yd
+
+        return v
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import datetime
+
 from PIL import Image
 from kivy.base import EventLoop
 from kivy.properties import NumericProperty, StringProperty, ObjectProperty
 from kivymd.app import MDApp
 from kivymd.toast import toast
 from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.card import MDCard
 from pyzbar.pyzbar import decode
 from camera4kivy import Preview
 from kivy.core.window import Window
@@ -12,12 +15,29 @@ from kivy import utils
 from kivymd.uix.tab import MDTabsBase
 from kivymd.uix.textfield import MDTextField
 
+from database import DataBase as DB
+
 Window.keyboard_anim_args = {"d": .2, "t": "linear"}
 Window.softinput_mode = "below_target"
 Clock.max_iteration = 250
 
 if utils.platform != 'android':
     Window.size = (412, 732)
+
+
+class RowCard(MDCard):
+    date = StringProperty("")
+    icon = StringProperty("")
+    cate = StringProperty("")
+    name = StringProperty("")
+    price = StringProperty("")
+
+    def price_symb(self, cat, prc):
+        if cat == "expenses":
+            self.price = "-" + prc + "/="
+        else:
+            self.price = "+" + prc + "/="
+        return self.price
 
 
 class Scan_Analyze(Preview):
@@ -70,6 +90,10 @@ class Tab(MDBoxLayout, MDTabsBase):
     pass
 
 
+class DictionaryProperty:
+    pass
+
+
 class MainApp(MDApp):
     size_x, size_y = Window.size
 
@@ -80,8 +104,10 @@ class MainApp(MDApp):
 
     # Cash data
     product_sold = StringProperty("0")
-    today_amount = StringProperty("0")
     total_amount = StringProperty("0")
+    date_frm = StringProperty(DB.date_format(DB()))
+    today_sold = StringProperty("0")
+    today_amount = StringProperty("0")
 
     # product details
     product_name = StringProperty("")
@@ -90,9 +116,15 @@ class MainApp(MDApp):
     product_barcode = StringProperty("")
     product_exp_year = StringProperty("")
     product_exp_month = StringProperty("")
+    data = DictionaryProperty()
+
+    # Selling data
+    amount_sold = StringProperty("")
+    quantity_sold = StringProperty("")
 
     def on_start(self):
         self.keyboard_hooker()
+        self.works()
 
     def keyboard_hooker(self, *args):
         EventLoop.window.bind(on_keyboard=self.hook_keyboard)
@@ -111,6 +143,106 @@ class MainApp(MDApp):
         elif key == 27 and self.screens_size == 0:
             toast('Press Home button!')
             return True
+
+    """
+            PRODUCT FUNCTIONS
+    
+    """
+
+    def save_product(self, product_name, product_price, product_quantity, product_barcode, product_exp_year,
+                     product_exp_month):
+        DB.save_product(DB(), product_name, product_price, product_quantity, product_barcode, product_exp_year,
+                        product_exp_month)
+
+    def caller_details(self):
+        Clock.schedule_once(self.get_product_details, .2)
+
+    def get_product_details(self, *args):
+        product_id = self.root.ids.sell_preview.text
+        self.data = DB.get_product(DB(), product_id)
+
+        self.product_name = self.data["product_name"]
+        self.product_price = self.data["product_price"]
+        self.product_quantity = self.data["product_quantity"]
+        self.product_barcode = self.data["product_barcode"]
+
+        print(self.data)
+
+    def amount_calc(self):
+        qnty = self.root.ids.quantity_sell.text
+        amnt = self.root.ids.sold_amount
+
+        if qnty != "" and self.product_price != "":
+            amnt.text = str(int(qnty) * int(self.product_price)) + "/="
+        else:
+            amnt.text = "0/="
+
+    def sell_product(self):
+        self.amount_sold = self.root.ids.sold_amount.text
+        self.quantity_sold = self.root.ids.quantity_sell.text
+
+        DB.sell_product(DB(), self.amount_sold, self.quantity_sold, self.product_barcode, self.product_name)
+
+    """
+            DATA FUNCTION
+    
+    """
+    # small vars
+    counter = 0
+    count = 0
+
+    def add_item(self, *args):
+        self.root.ids.customers.data = {}
+        main = DB.get_all_today(DB())
+        print(main)
+        if main:
+            for i, y in main.items():
+                self.today_amount = str(int(y["sold_amount"].replace("/=", "")) + int(self.today_amount))
+                if self.counter < 9:
+                    self.root.ids.customers.data.append(
+                        {
+                            "viewclass": "RowCard",
+                            "icon": "shopping-outline",
+                            "name": y["product_name"],
+                            "price": f"{y['sold_amount']}/=",
+                            "id": i
+                        }
+                    )
+                else:
+                    if self.count == 0:
+                        self.root.ids.customers.data.append(
+                            {
+                                "viewclass": "More",
+                                "icon": "dots-horizontal"
+                            }
+                        )
+                        self.count = + 1
+                self.counter = self.counter + 1
+                self.today_sold = str(self.counter)
+        else:
+            img = self.root.ids.nodata
+            img.source = "components/icons/file-plus.jpg"
+
+    def stream_work(self, message):
+        if True:
+            print("working!")
+            try:
+                Clock.schedule_once(self.add_item, .2)
+            except:
+                pass
+
+    def works(self):
+        try:
+            import firebase_admin
+            firebase_admin._apps.clear()
+            from firebase_admin import credentials, initialize_app, db
+            cred = credentials.Certificate("credential/farmzon-abdcb-c4c57249e43b.json")
+            initialize_app(cred, {'databaseURL': 'https://farmzon-abdcb.firebaseio.com/'})
+            self.my_stream = db.reference('ShopCode').child("Sold").child(str(datetime.datetime.now().date().year)).child(
+                    DB.date_format(DB())).listen(
+                self.stream_work)
+        except:
+            print("Woks sorry!!!")
 
     """
     
@@ -137,6 +269,32 @@ class MainApp(MDApp):
                 self.product_barcode = barcode
 
                 self.root.ids.result_preview.text = barcode
+
+        else:
+            print("Reaaly")
+
+    def sell_preview(self):
+        self.root.ids.preview_sell.connect_camera(enable_analyze_pixels=True, default_zoom=0.0)
+        print("connected")
+
+    def stop_camera_sell(self):
+        self.root.ids.preview_sell.disconnect_camera()
+
+    @mainthread
+    def get_barcode_sell(self, result):
+        barcode = str(result.data)
+        code_type = str(result.type)
+        print(barcode)
+        if barcode:
+            if code_type != "QRCODE":
+                barcode = barcode.replace("b", "").replace("'", "")
+
+                self.product_barcode = barcode
+
+                self.root.ids.sell_preview.text = barcode
+
+        else:
+            print("Reaaly")
 
     """
 
